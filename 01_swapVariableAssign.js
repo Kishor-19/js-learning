368e1fe68dead3ff17e2783141e1e5369cfb3d2d
Kishor-19 <125597456+Kishor-19@users.noreply.github.com>
2024-03-08 22:59:54 +0530

    swap and variable
    01_Swap variable
    01_Assign Variable

---
+++ 01_swapVariableAssign.js
@@ -0,0 +1,36 @@
+console.log("Swap the Two variable using third variable");
+var cutie;
+cutie="Cutie"
+var sweety;
+sweety ="Sweety "
+console.log("======Before Swap======");
+console.log("Sweety :",sweety,"Cutie :",cutie);
+
+console.log("======After Swap======");
+var term=sweety;
+var sweety=cutie;
+var cutie=term
+ console.log("Sweety:",sweety);
+ console.log("Cutie:",cutie);
+
+
+ console.log("Swap three values");
+
+ var num1;
+ num1=100;
+ var num2;
+ num2=200;
+ var num3;
+ num3=300;
+ console.log("=====Before=====" );
+ console.log("num1:",num1);
+ console.log("num2:",num2);
+ console.log("num3:",num3);
+ console.log("======After Swap=====");
+ var temp=num1;
+ var num1=num2;
+ var num2=num3;
+ var num3=temp;
+ console.log("num1",num1);
+ console.log("num2",num2);
+ console.log("num3",temp);
---
+++ 01_variableAssign.js
@@ -0,0 +1,48 @@
+console.log('======Step 1======');
+
+var myFullName;
+console.log("My Full Name  is :", myFullName);
+myFullName=" My full Name is: Kishor Dendage";
+console.log("After initilization");
+console.log(myFullName);
+console.log("After Update the Name:");
+var myFullName;
+myFullName="Kishor Santosh Dendage"
+console.log("My Full Name is:",myFullName);
+console.log("======Step 2======");
+var mylove;
+mylove="I love only JS"
+console.log(mylove);
+console.log("=====Step 3=====");
+
+var first;
+first=" Kishor"
+console.log("First Name:",first);
+
+var last;
+last=" Dendage"
+console.log( "Last Name :",last);
+
+var birth;
+birth='22'
+console.log("Age :",birth);
+
+var dress;
+dress="A-Nagar"
+console.log("Address",dress);
+
+var pin;
+pin="413702"
+console.log("Pincode :",pin);
+
+var gender;
+gender="Male"
+console.log("Gender :",gender);
+
+var married;
+married="No"
+console.log("isMarried :",married);
+
+
+
+
