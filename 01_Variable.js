5dcb2727612492ad2fa08e530957179ed165d8a5
Kishor-19 <125597456+Kishor-19@users.noreply.github.com>
2024-03-09 22:21:45 +0530

    typeOf ,js
    Data Types

---
+++ 01_Variable.js
@@ -0,0 +1,13 @@
+var city;
+city="Pune";
+console.group(city);
+console.log("My city Name is:", city);
+
+console.log("What is undefined ?");
+var firstName; 
+console.log("First Name variable value before Initialization:", firstName);
+firstName="Gajanan";
+console.log("First name:", firstName);
+
+firstName = "Ratan";
+console.log("First name:", firstName);
---
+++ 02_typeOf.js
@@ -0,0 +1,44 @@
+console.log("=====Step 1====");
+var bankName;
+console.log("Bank Name :",bankName);
+console.log("==After Initilization===");
+
+var bankName="SBI Bank"
+console.log("Bank Name :",bankName);
+typeofbankName= "SBI Bank"
+var typeofbankName = typeof bankName;
+console.log("Data Type :",typeofbankName);
+
+console.log("====Step 2====");
+
+var marks= '90%'
+console.log("Marks :",marks);
+typeofmarks='90%'
+var typeofmarks = typeof typeofmarks;
+console.log("Data Type :",typeofmarks);
+
+console.log("====Step 3====");
+
+var isWorking;
+console.log("isWorking :",isWorking);
+typeOfisWorking = typeof isWorking;
+console.log("Data Type :",typeOfisWorking);
+
+console.log("After Initilization");
+var isWorking=true;
+console.log("isWorking :",isWorking);
+  typeOfisWorking = typeof isWorking;
+ console.log("Data Type :",typeOfisWorking);
+
+
+console.log("====Step 4====");
+var totalCount="Hundred and Seven"
+console.log("Total Count :",totalCount);
+typeOftotalCount =typeof totalCount;
+console.log("Data Type :",typeOftotalCount);
+
+console.log("After Update ");
+var totalCount=107;
+console.log("Total Count :",totalCount);
+typeOftotalCount = typeof totalCount;
+console.log("Data Type :",typeOftotalCountdesk);
